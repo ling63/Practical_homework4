b30b4810bb9b3485f6b1b275c1d26434af384c2c
ling63 <liewsl-wm19@student.tarc.edu.my>
2020-07-10 16:31:39 +0800

    commit by ling 10/7/2020

--- app/src/main/java/com/example/practical_homework_4/MainActivity.java
+++ app/src/main/java/com/example/practical_homework_4/MainActivity.java
@@ -9,45 +9,51 @@ import android.widget.Toast;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private CheckBox chocolate_syrup,sprinkles,crushed_nuts,charries,orio_cookies;
+    private Button show_toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButtonClick();
     }
 
-    public void addListenerOnButtonClick(){
-        CheckBox chocolate_syrup = (CheckBox) findViewById(R.id.chk_chocolate_syrup);
-        CheckBox sprinkles = (CheckBox) findViewById(R.id.chkSprinkles);
-        CheckBox crushed_nuts = (CheckBox) findViewById(R.id.chk_Crushed);
-        CheckBox charries = (CheckBox) findViewById(R.id.chk_Cherries);
-        CheckBox orio_cookies = (CheckBox) findViewById(R.id.chk_Orio);
-        Button show_toast = (Button) findViewById(R.id.toast);
-        show_toast.setOnClickListener((View view){
-            StringBuilder result = new StringBuilder();
-            result.append("Toppings:");
+    public void addListenerOnButtonClick()
+    {
+        chocolate_syrup = (CheckBox) findViewById(R.id.chk_chocolate_syrup);
+        sprinkles = (CheckBox) findViewById(R.id.chkSprinkles);
+        crushed_nuts = (CheckBox) findViewById(R.id.chk_Crushed);
+        charries = (CheckBox) findViewById(R.id.chk_Cherries);
+        orio_cookies = (CheckBox) findViewById(R.id.chk_Orio);
+        show_toast = (Button) findViewById(R.id.toast);
+        show_toast.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view) {
 
-            if(chocolate_syrup.isChecked()){
-                result.append("Chocolate Syrup" + "");
-            }
+                StringBuffer result = new StringBuffer();
+                result.append("Toppings:");
 
-            if(sprinkles.isChecked()){
-                result.append("Sprinkles" + "");
-            }
+                if (chocolate_syrup.isChecked()) {
+                    result.append("Chocolate Syrup" + "");
+                }
 
-            if(crushed_nuts.isChecked()){
-                result.append("Crushed Nuts" + "");
-            }
+                if (sprinkles.isChecked()) {
+                    result.append("Sprinkles" + "");
+                }
 
-            if(charries.isChecked()){
-                result.append("Cherries" +"");
-            }
+                if (crushed_nuts.isChecked()) {
+                    result.append("Crushed Nuts" + "");
+                }
 
-            if(orio_cookies.isChecked()){
-                result.append("Orio Cookie Crumbles" +"");
+                if (charries.isChecked()) {
+                    result.append("Cherries" + "");
+                }
+
+                if (orio_cookies.isChecked()) {
+                    result.append("Orio Cookie Crumbles" + "");
+                }
+                Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
             }
-            Toast.makeText(getApplicationContext(),result.toString(),Toast.LENGTH_SHORT).show();
         });
     }
 
